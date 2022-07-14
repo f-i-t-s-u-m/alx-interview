3315809b90b2c0484a37b49284740ac4321b22b2
f-i-t-s-u-m <fibabirr@gmail.com>
2022-07-14 13:44:44 +0300

    fix function error

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -7,7 +7,7 @@ def validUTF8(data):
     n_bytes = 0
 
     for num in data:
-        print(format(num, "#010b"))
+        binary = format(num, "#010b")[-8:]
         if n_bytes == 0:
             for bit in binary:
                 if bit == '0':
