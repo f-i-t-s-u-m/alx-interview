7f73b543abcc179535b9b48908d2279d93ee1ba3
f-i-t-s-u-m <fibabirr@gmail.com>
2022-07-14 13:34:20 +0300

    using bytes module

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -4,10 +4,23 @@
 
 def validUTF8(data):
     """ validUTF8 function """
-    bo = True
-    for i in data:
-        try:
-            bytes.fromhex(hex(i)[2:]).decode()
-        except ValueError:
-            bo = False
-    return bo
+    def validUTF8(data):
+    n_bytes = 0
+
+    for num in data:
+        binary = format(num, "#010b")[-8:]
+        if n_bytes == 0:
+            for bit in binary:
+                if bit == '0':
+                    break
+                n_bytes += 1
+            if n_bytes == 0:
+                continue
+            if n_bytes == 1 or n_bytes > 4:
+                return False
+        else:
+            if binary[0] != '1' or binary[1] != '0':
+                return False
+
+        n_bytes -= 1
+    return n_bytes == 0
