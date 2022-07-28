45e7031616e986ed9cde486271dd72473c25e7d6
f-i-t-s-u-m <fibabirr@gmail.com>
2022-07-29 01:54:59 +0300

    init project

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -5,14 +5,16 @@ const id = process.argv[2];
 const req = require('request');
 const url = 'https://swapi-api.hbtn.io/api/films/' + id;
 req(url, async function (err, resp, body) {
+  if (err) return console.error(err);
   if (!err && resp.statusCode === 200) {
     const characters = JSON.parse(body).characters;
     for (const a of characters) {
       await new Promise(function (resolve, reject) {
         req(a, function (er, res, body) {
+          if (er) return console.error(er);
           console.log(JSON.parse(body).name);
+          resolve();
         });
-        resolve();
       });
     }
   }
